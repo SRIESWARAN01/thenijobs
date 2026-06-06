a39c8e78dbe3a0e26e085e3b121d8c995d554693
SRIESWARAN01 <srieswaran01@users.noreply.github.com>
2026-06-06 17:29:59 +0530

    Update Next config

--- thenijobs-main/next.config.ts
+++ thenijobs-main/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  experimental: {
-    webpackBuildWorker: false,
-  },
   images: {
     unoptimized: true,
     remotePatterns: [
